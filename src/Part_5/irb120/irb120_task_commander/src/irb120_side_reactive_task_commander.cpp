0fd827742a787eaf34af8b951cc8fa538323f290
hanbincho <hxc431@case.edu>
2018-10-26 09:51:01 -0400

    Completed PS5

--- src/Part_5/irb120/irb120_task_commander/src/irb120_side_reactive_task_commander.cpp
+++ src/Part_5/irb120/irb120_task_commander/src/irb120_side_reactive_task_commander.cpp
@@ -247,13 +247,11 @@ int main(int argc, char** argv) {
     ROS_INFO("done with first trajectory");
     //xxxxxxxxxxxxxxxxxx
 
-    // SECOND TRAJECTORY MOVEMENT- MOVE THE ARM NEXT TO THE GEAR AND GET READY TO MOVE IT IN THE X-DIRECTION
+    // SECOND TRAJECTORY MOVEMENT- MOVE THE ARM NEXT TO THE GEAR AND GET READY TO MOVE IT IN THE Y-DIRECTION
     if (goal_object_pose.pose.position.y <= g_perceived_object_pose.pose.position.y) {
-    	//flange_origin << flange_origin(0) + 0.08, g_perceived_object_pose.pose.position.y, 0.0; 
 	flange_origin << flange_origin(0), g_perceived_object_pose.pose.position.y + 0.08, 0.0;
     }
     else {
-    	//flange_origin << flange_origin(0) - 0.08, g_perceived_object_pose.pose.position.y, 0.0;
 	flange_origin << flange_origin(0), g_perceived_object_pose.pose.position.y - 0.08, 0.0;
     }
     goal_flange_affine.translation() = flange_origin;
@@ -277,7 +275,7 @@ int main(int argc, char** argv) {
     ros::Duration(arrival_time).sleep(); //wait for the motion
     ROS_INFO("done with second trajectory");
 
-    // THIRD TRAJECTORY MOVEMENT- MOVE THE GEAR A CERTAIN DISTANCE IN THE X-DIRECTION, IN THIS CASE, MOVE IT 0.01 DISPLACEMENT
+    // THIRD TRAJECTORY MOVEMENT- MOVE THE GEAR A CERTAIN DISTANCE IN THE Y-DIRECTION
     if (goal_object_pose.pose.position.y <= g_perceived_object_pose.pose.position.y) {
     	//flange_origin << goal_object_pose.pose.position.x + 0.08, g_perceived_object_pose.pose.position.y, 0.0; 
 	flange_origin << g_perceived_object_pose.pose.position.x, goal_object_pose.pose.position.y + 0.08, 0.0; 
@@ -308,13 +306,11 @@ int main(int argc, char** argv) {
 
     object_finder_ac.sendGoal(object_finder_goal,&objectFinderDoneCb); //request object finding via action server
 
-    // FOURTH TRAJECTORY MOVEMENT- MOVE THE ARM NEXT TO THE GEAR AND GET READY TO MOVE IT IN THE Y-DIRECTION
+    // FOURTH TRAJECTORY MOVEMENT- MOVE THE ARM NEXT TO THE GEAR AND GET READY TO MOVE IT IN THE X-DIRECTION
     if (goal_object_pose.pose.position.x <= g_perceived_object_pose.pose.position.x) {
-    	//flange_origin << g_perceived_object_pose.pose.position.x + 0.08, flange_origin(1), 0.2; 
 	flange_origin << g_perceived_object_pose.pose.position.x+0.08, g_perceived_object_pose.pose.position.y, 0.1; 
     }
     else {
-    	//flange_origin << g_perceived_object_pose.pose.position.x - 0.08, flange_origin(1), 0.2;
 	flange_origin << g_perceived_object_pose.pose.position.x-0.08, g_perceived_object_pose.pose.position.y, 0.1;
     }
     goal_flange_affine.translation() = flange_origin;
@@ -338,8 +334,7 @@ int main(int argc, char** argv) {
     ros::Duration(arrival_time).sleep(); //wait for the motion
     ROS_INFO("done with fourth trajectory");
 
-    // FIFTH TRAJECTORY MOVEMENT- MOVE THE GEAR A CERTAIN DISTANCE IN THE Y-DIRECTION, IN THIS CASE, MOVE IT 0.01 DISPLACEMENT
-    //go to pose to touch top of gear part; same x and y, but lower z value for flange origin
+    // FIFTH TRAJECTORY MOVEMENT- MOVE THE GEAR A CERTAIN DISTANCE IN THE X-DIRECTION
     if (goal_object_pose.pose.position.x <= g_perceived_object_pose.pose.position.x) {
     	flange_origin << goal_object_pose.pose.position.x + 0.08, g_perceived_object_pose.pose.position.y, 0.0; 
     }
