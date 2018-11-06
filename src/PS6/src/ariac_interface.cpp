0c24dc9bf2090cfadbadffd154f7600ba6f67978
hanbincho <hxc431@case.edu>
2018-11-05 19:57:44 -0500

    Added portions of logical camera

--- src/PS6/src/ariac_interface.cpp
+++ src/PS6/src/ariac_interface.cpp
@@ -4,8 +4,9 @@
 #include<ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include<std_srvs/Trigger.h>
-#include<osrf_gear/ConveyorBeltControl>
-#include<osrf_gear/DroneControl>
+#include<osrf_gear/ConveyorBeltControl.h>
+#include<osrf_gear/DroneControl.h>
+#include<osrf_gear/LogicalCameraImage.h>
 
 
 // This function will be called once when the goal completes
@@ -20,14 +21,14 @@ int main(int argc, char** argv) {
     osrf_gear::DroneControl drone_srv;
 
     // Create action clients for startup, conveyor and drone
-    actionlib::SimpleActionClient<std_srvs::Trigger> action_client("/ariac/start_competition", true);
-    actionlib::SimpleActionClient<osrf_gear::ConveyorBeltControl> action_client("/ariac/conveyor/control", true);
-    actionlib::SimpleActionClient<osrf_gear::DroneControl> action_client("/ariac/drone", true);
+    //actionlib::SimpleActionClient<std_srvs::Trigger> action_client("/ariac/start_competition", true);
+    actionlib::SimpleActionClient<osrf_gear::ConveyorBeltControl> ("/ariac/conveyor/control", true);
+    actionlib::SimpleActionClient<osrf_gear::DroneControl> ("/ariac/drone", true);
 
     // Initialize the response to start up Gazebo and the simulation
-    startup_srv.response.success = false;
-    startup_client.call(startup_srv);
-
+    //startup_srv.response.success = false;
+    //startup_client.call(startup_srv);
+/*
     // Check if there is a successful startup, if not try again
     while(!startup_srv.response.sucess) {
     	ROS_WARN("not successful starting up");
@@ -35,7 +36,8 @@ int main(int argc, char** argv) {
     	ros::Duration(0.5).sleep();
     }
     ROS_INFO("startup response is successful");
-
+*/
+/*
     // Initialize the response for the conveyor
     conveyor_srv.response.success = false;
     // Initialize power level to see conveyor move
@@ -65,7 +67,17 @@ int main(int argc, char** argv) {
     	ros::Duration(0.5).sleep();
     }
     ROS_INFO("conveyor response is successful");
+*/
 
+void cam2B(const osrf_gear::LogicalCameraImage& message_holder) {
+	bool g_take_new_snapshot = false;
+	// need to see what type should be here osrf_gear:: "" g_cam1_data;
+    	if(g_take_new_snapshot) {
+    	}
+    	ROS_INFO_STREAM("image from camera " << message_holder << endl);
+	g_cam1_data = message_holder;
+	g_take_new_snapshot = false;
+}
     return 0;
 }
 
